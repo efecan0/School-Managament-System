7300a7b5583234702662dbdc5ce956aebc837819
Efecan erdem <efecanerdem0907@gmail.com>
2022-04-02 21:29:26 +0300

    user model changed, 2 to 1

---
+++ models/user.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
+    schoolID: {
+        type    : mongoose.Schema.Types.ObjectId,
+        default : mongoose.Types.ObjectId,
+        index   : {
+             unique: true
+             }
+    },
+    manager: {
+      type: Boolean,
+      default: false
+
+    },
+    lessons: []
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
