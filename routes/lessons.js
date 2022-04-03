303789ffd53ad17b2af86d835c1c7bb09e924a62
Efecan erdem <efecanerdem0907@gmail.com>
2022-04-03 15:05:19 +0300

    lesson update endpoint added

--- routes/lessons.js
+++ routes/lessons.js
@@ -11,7 +11,6 @@ router.route('/')
         res.json(lesson);
     }, (err) => next(err)).catch((err) => next(err))
 }).post((req, res, next) => {
-    console.log(req.body)
     Lesson.create({"name": req.body.name, "quota": req.body.quota})
     .then((lesson) => {
         console.log('lesson created');
@@ -19,6 +18,35 @@ router.route('/')
         res.setHeader("Content-type", "application/json");
         res.json(lesson)
     }, (err) => next(err)).catch((err) => next(err))
+}).put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation is not supported on /lessons")
+}).delete((req, res, next) => {
+    res.statusCode = 403;
+    res.end("DELETE operation is not supported on /lessons")
+})
+
+router.route('/:lesson')
+.get((req, res, next) => {
+    Lesson.find({name: req.params.lesson})
+    .then((lesson) => {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(lesson);
+    }, (err) => next(err)).catch((err) => next(err))
+}).post((req, res, next) => {
+    res.statusCode = 403;
+    res.end("POST operation is not supported on /lessons/"+req.params.lesson)
+}).put( async (req, res, next) => {
+    var lesson = await Lesson.findOne({name: req.params.lesson});
+    lesson.quota = req.body.quota;
+    lesson.save()
+    .then((lesson) => {
+        console.log(req.params.lesson+'lesson has updated');
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        res.json(lesson)
+    })
 })
 
 module.exports = router
